c4bd2ac5f3709583ed814b2c1e316cbc5fb6c8d3
NKoroloff <52699527+NKoroloff@users.noreply.github.com>
2019-10-24 11:32:39 +0300

    ES6

--- ES 5/js/main.js
+++ ES 5/js/main.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     let pushBtn = document.getElementById('push__btn');
     let popBtn = document.getElementById('pop__btn');
-    /*let pushInput = document.getElementById('push__input').value;*/
+    let pushInput = document.getElementById('push__input').value;
     let popInput = document.getElementById('pop__input').value;
 
 
     let presentation = document.querySelector('lList-noda');
 
-    pushBtn.addEventListener("click", function () {
-        pushToPresentation();
+    pushBtn.addEventListener("click", function (e) {
+        setTimeout(function (){
+            pushToPresentation();
+        },600);
+
         console.log(lList);
     });
 
-    popBtn.addEventListener("click", function () {
+    popBtn.addEventListener("click", function (e) {
         popFromPresentation();
         console.log(lList);
     });
@@ -20,10 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function pushToPresentation() {
         let pushInput = document.getElementById('push__input').value;
-        let node = document.getElementsByClassName("lList-noda");
-        node[0].insertAdjacentHTML("beforeend",
+        let nodeRoot = document.getElementsByClassName("lList-noda");
+        nodeRoot[0].insertAdjacentHTML("beforeend",
             `<div class="nodes">${pushInput} </div>`);
         lList.push(pushInput);
+
 
     };
 
